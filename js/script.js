e1d71fdc402ce6daa5b0039ccf8a746a4393d1b8
Rishi Raj <rishiraj824@gmail.com>
2016-12-20 09:09:57 +0530

    init

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+ 
+  $("#partners").owlCarousel({
+ 
+      autoPlay: false, //Set AutoPlay to 3 seconds
+ 
+      items : 3,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  }); 
+});
